b7eb8d155a427f47961283bb09b36a6bd47709f3
YUGSHUKLA <YUGSHUKLA@192.168.0.19>
2018-06-12 02:22:02 +0530

    i add one forloop functionallity

--- GitHubDemo/src/com/nit/gitdemo/GitHubDemo.java
+++ GitHubDemo/src/com/nit/gitdemo/GitHubDemo.java
@@ -4,6 +4,9 @@ public class GitHubDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To GitHub");
+		for(int i=0;i<=10;i++) {
+			System.out.println("I::"+i);
+		}
 
 	}
 
