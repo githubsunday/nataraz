5f5eb40c28385e83f5caa91c5d64c949d1fa2d5a
YUGSHUKLA <YUGSHUKLA@192.168.0.19>
2018-06-12 02:28:28 +0530

    added one m1 method

--- GitHubDemo/src/com/nit/gitdemo/GitHubDemo.java
+++ GitHubDemo/src/com/nit/gitdemo/GitHubDemo.java
@@ -1,12 +1,17 @@
 package com.nit.gitdemo;
 
 public class GitHubDemo {
+	public static void m1() {
+		System.out.println("M1 Method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To GitHub");
 		for(int i=0;i<=10;i++) {
 			System.out.println("I::"+i);
 		}
+		
+		m1();
 
 	}
 
