b05d33b69dc073e80355ee60518844286785bb44
HP <HP@192.168.0.7>
2018-06-15 12:53:51 +0530

    method to created by shakti vishvakrma.this method i have done 1 to 10 all number squre print.

--- GitHubDemo/src/com/nit/gitdemo/GitHubDemo.java
+++ GitHubDemo/src/com/nit/gitdemo/GitHubDemo.java
@@ -2,17 +2,21 @@ package com.nit.gitdemo;
 
 public class GitHubDemo {
 	public static void m1() {
-		System.out.println("M1 Method");
+		//System.out.println("M1 Method");
 	}
-
+    public static void m2(int i)
+    {
+    	System.out.print(i*i+" ");
+    }
 	public static void main(String[] args) {
 		System.out.println("Welcome To GitHub");
 		for(int i=0;i<=10;i++) {
-			System.out.println("I::"+i);
+			//System.out.println("I::"+i);
+			 m2(i);//created by shakti
 		}
 		
 		m1();
-
+   
 	}
 
 }
